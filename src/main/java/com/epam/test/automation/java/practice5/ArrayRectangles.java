af06ea2f8adfa458a7873a55fca8f643b0efcf91
Mykola <legisus@gmail.com>
2021-07-27 16:55:22 +0200

    2

--- src/main/java/com/epam/test/automation/java/practice5/ArrayRectangles.java
+++ src/main/java/com/epam/test/automation/java/practice5/ArrayRectangles.java
@@ -1,9 +1,5 @@
 package com.epam.test.automation.java.practice5;
 
-import java.lang.reflect.Array;
-import java.util.ArrayList;
-import java.util.List;
-
 /**
  * <summary>
  * Implement class according to description of task.
@@ -11,14 +7,14 @@ import java.util.List;
  */
 
 public class ArrayRectangles {
-    //TODO: Delete line below and write your own solution;
-    //TODO: implement constructors according to description of task
-    private int n;
-    List<Rectangle> rectangleArray = new ArrayList<Rectangle>();
+    private Rectangle[] rectangles;
 
+    public ArrayRectangles(int n) {
+        this.rectangles = new Rectangle[n];
+    }
 
-    public ArrayRectangles(Array[] rectangleArray, int n) {
-        this.n = n;
+    public ArrayRectangles(Rectangle... rectangles) {
+        this.rectangles = rectangles;
     }
 
     /**
@@ -27,13 +23,13 @@ public class ArrayRectangles {
      * </summary>
      */
     public boolean addRectangle(Rectangle rectangle) {
-        if (rectangleArray.get(n) == null){
-
-            rectangleArray.add(rectangle);
-            return true;
-        }else {
-            return false;
+        for (int i = 0; i < rectangles.length; i++) {
+            if (rectangles[i] == null) {
+                rectangles[i] = rectangle;
+                return true;
+            }
         }
+        return false;
     }
 
     /**
@@ -42,8 +38,15 @@ public class ArrayRectangles {
      * </summary>
      */
     public int numberMaxArea() {
-        //TODO: Delete line below and write your own solution;
-        throw new UnsupportedOperationException();
+        double maxArea = 0;
+        int index = -1;
+        for (int i = 0; i < rectangles.length; i++) {
+            if (maxArea < rectangles[i].area()) {
+                maxArea = rectangles[i].area();
+                index = i;
+            }
+        }
+        return index;
     }
 
     /**
@@ -52,8 +55,21 @@ public class ArrayRectangles {
      * </summary>
      */
     public int numberMinPerimeter() {
-        //TODO: Delete line below and write your own solution;
-        throw new UnsupportedOperationException();
+        if (rectangles.length == 0) {
+            return -1;
+        }
+        double minPerimeter = rectangles[0].perimeter();
+
+        int index = -1;
+        for (int i = 1; i < rectangles.length; i++) {
+
+            if (minPerimeter > rectangles[i].perimeter()) {
+                minPerimeter = rectangles[i].perimeter();
+                index = i;
+            }
+        }
+        return index;
+
     }
 
     /**
@@ -62,8 +78,14 @@ public class ArrayRectangles {
      * </summary>
      */
     public int numberSquares() {
-        //TODO: Delete line below and write your own solution;
-        throw new UnsupportedOperationException();
+        int numberSquares = 0;
+        for (int i = 0; i < rectangles.length; i++) {
+            if (rectangles[i].isSquare()) {
+                numberSquares++;
+            }
+        }
+        return numberSquares;
     }
+
 
 }
